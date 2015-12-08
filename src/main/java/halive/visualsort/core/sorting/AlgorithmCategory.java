d13f6751b32b721fd9bfa8727a2ecc1f2bfdcd0d
HALive <HALive1996@gmail.com>
2015-12-08 22:33:31 +0100

    Implemented HeapSort

---
+++ src/main/java/halive/visualsort/core/sorting/AlgorithmCategory.java
@@ -0,0 +1,9 @@
+package halive.visualsort.core.sorting;
+
+/**
+ * This Enum defines Different Categories of Sorting Algorithms
+ *
+ */
+public enum AlgorithmCategory {
+    RECURSIVE, ITERATIVE, JOKES;
+}
--- src/main/java/halive/visualsort/core/sorting/impl/HeapSort.java
+++ src/main/java/halive/visualsort/core/sorting/impl/HeapSort.java
@@ -8,8 +8,37 @@ public class HeapSort extends SortingAlgorithm {
     public HeapSort() {
         super("HeapSort", " ");
     }
+
     @Override
     public void doSort(DataEntry[] data, SortingHandler sortingHandler) {
-        //TODO IMPLEMENT HEAPSORT!
+        createHeap(data, sortingHandler);
+        for (int i = data.length - 1; i > 0; i--) {
+            sortingHandler.swap(0, i);
+            createHeapK(data, 0, i - 1, sortingHandler);
+        }
+    }
+
+    public void createHeap(DataEntry[] data, SortingHandler a) {
+        for (int i = (data.length / 2); i >= 0; i--) {
+            createHeapK(data, i, data.length - 1, a);
+        }
+    }
+
+    public void createHeapK(DataEntry[] data, int knot, int size, SortingHandler a) {
+        int left = (knot * 2) + 1;
+        int right = left + 1;
+        int middle;
+        if (left <= size && right > size) {
+            if (data[left].getValue() < data[knot].getValue()) {
+                a.swap(left, knot);
+            }
+        } else if (right <= size) {
+            middle = a.compare(data[left].getValue() > data[right].getValue()) ? left : right;
+            if (a.compare(data[middle].getValue() > data[knot].getValue())) {
+                a.swap(middle, knot);
+                createHeapK(data, middle, size, a);
+            }
+        }
+
     }
 }

65503f061c007a265b454ecca92debb993ddedfb
HALive <HALive1996@gmail.com>
2015-12-27 16:32:02 +0100

    Fixed Bitonic Merge Sort

--- corePlugin/src/main/java/halive/visualsort/sortingalgorithms/mergesort/BitonicMergeSort.java
+++ corePlugin/src/main/java/halive/visualsort/sortingalgorithms/mergesort/BitonicMergeSort.java
@@ -5,39 +5,67 @@
 
 package halive.visualsort.sortingalgorithms.mergesort;
 
+import halive.visualsort.core.DataEntry;
 import halive.visualsort.core.SortingHandler;
+import halive.visualsort.core.sorting.SortingAlgorithm;
 
 /**
  * This CLass implements Bitonic Merge Sort.
+ * The Implementation has been Taken from:
+ * http://www.iti.fh-flensburg.de/lang/algorithmen/sortieren/bitonic/oddn.htm
  */
-public class BitonicMergeSort extends MergeSort {
+public class BitonicMergeSort extends SortingAlgorithm {
+
+    public BitonicMergeSort() {
+        super("Bitonic Merge Sort", "");
+    }
 
     @Override
-    public void merge(int lo, int hi, int m, int[] a, int[] b, SortingHandler h) {
-        int i, j, k;
+    public void doSort(DataEntry[] data, SortingHandler sortingHandler) {
+        bitonicMergeSort(0, data.length, ASC, data, sortingHandler);
+    }
 
-        for (i = lo; h.compare(i <= hi); i++) {
-            b[i] = a[i];
+    private void bitonicMergeSort(int l, int r, boolean direction, DataEntry[] entries, SortingHandler handler) {
+        if (r > 1) {
+            int middle = r / 2;
+            bitonicMergeSort(l, middle, !direction, entries, handler);
+            bitonicMergeSort(l + middle, r - middle, direction, entries, handler);
+            merge(l, r, direction, entries, handler);
         }
-        i = lo;
-        j = m + 1;
-        k = lo;
-        while (h.compare(i <= m && j <= hi)) {
-            if (h.compare(b[i] <= b[j])) {
-                a[k++] = b[i++];
-            } else {
-                a[k++] = b[j++];
+    }
+
+    private void merge(int l, int r, boolean direction, DataEntry[] entries, SortingHandler handler) {
+        if (r > 1) {
+            int middle = determineMiddleForMerge(r);
+            for (int i = l; i < (l + r) - middle; i++) {
+                if (handler.compare(direction == (entries[i].getValue() > entries[i + middle].getValue()))) {
+                    handler.swap(i, i + middle);
+                }
             }
-            h.incrementSwapsAndDelay();
+            merge(l, middle, direction, entries, handler);
+            merge(l + middle, r - middle, direction, entries, handler);
         }
-        while (h.compare(i <= m)) {
-            a[k++] = b[i++];
-            h.incrementSwapsAndDelay();
+    }
+
+    public int determineMiddleForMerge(int r) {
+        int i = 1;
+        while (i < r) {
+            i = i << 1;
         }
+        return i >> 1;
+    }
+
+    @Override
+    public String getCategory() {
+        return "MergeSort";
     }
 
     @Override
     public String getName() {
         return "Bitonic Merge sort";
     }
+
+    //Direction Constants
+    private static boolean ASC = true;
+    private static boolean DESC = false;
 }
